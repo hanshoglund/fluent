d4a98a7aa88e09660ba631956a88ea8cca4fcbdd
Hans Höglund <hans@hanshoglund.se>
2014-11-30 19:57:19 +0000

    Misc

--- setupFluent.hs
+++ setupFluent.hs
@@ -1,13 +1,13 @@
 [
-  ("chord1_begin", "sounds/mono_chord1_begin.wav", (0,11))
-, ("chord2_begin", "sounds/mono_chord2_begin.wav", (0,11))
-, ("chord3_begin", "sounds/mono_chord3_begin.wav", (0,11))
-, ("chord4_begin", "sounds/mono_chord4_begin.wav", (0,11))
+  ("chord1_begin", "sounds/mono_chord4_begin_fade.wav", (0,11))
+, ("chord2_begin", "sounds/mono_chord2_begin_fade.wav", (0,11))
+, ("chord3_begin", "sounds/mono_chord3_begin_fade.wav", (0,11))
+, ("chord4_begin", "sounds/mono_chord4_begin_fade.wav", (0,11))
 
-, ("chord5_begin", "sounds/mono_chord5_begin.wav", (0,11))
-, ("chord6_begin", "sounds/mono_chord6_begin.wav", (0,11))
-, ("chord7_begin", "sounds/mono_chord7_begin.wav", (0,11))
-, ("chord8_begin", "sounds/mono_chord8_begin.wav", (0,11))
+, ("chord5_begin", "sounds/mono_chord5_begin_fade.wav", (0,11))
+, ("chord6_begin", "sounds/mono_chord6_begin_fade.wav", (0,11))
+, ("chord7_begin", "sounds/mono_chord7_begin_fade.wav", (0,11))
+, ("chord8_begin", "sounds/mono_chord8_begin_fade.wav", (0,11))
 
 , ("crossing_bass", "sounds/mono_crossing_bass_with_fade.wav", (0,25))
 ]
