845b07f22b463c2e76dfc67b797f3a6af05ed1cc
Hans Höglund <hans@hanshoglund.se>
2014-12-02 21:42:57 +0000

    Patch

--- setupFluent.hs
+++ setupFluent.hs
@@ -1,13 +1,30 @@
 [
-  ("chord1_begin", "sounds/mono_chord1_begin_fade.wav", (0,11))
-, ("chord2_begin", "sounds/mono_chord2_begin_fade.wav", (0,11))
-, ("chord3_begin", "sounds/mono_chord3_begin_fade.wav", (0,11))
-, ("chord4_begin", "sounds/mono_chord4_begin_fade.wav", (0,11))
+  ("chord1_begin_fade", "sounds/mono_chord1_begin_fade.wav", (0,11))
+, ("chord2_begin_fade", "sounds/mono_chord2_begin_fade.wav", (0,11))
+, ("chord3_begin_fade", "sounds/mono_chord3_begin_fade.wav", (0,11))
+, ("chord4_begin_fade", "sounds/mono_chord4_begin_fade.wav", (0,11))
 
-, ("chord5_begin", "sounds/mono_chord5_begin_fade.wav", (0,11))
-, ("chord6_begin", "sounds/mono_chord6_begin_fade.wav", (0,11))
-, ("chord7_begin", "sounds/mono_chord7_begin_fade.wav", (0,11))
-, ("chord8_begin", "sounds/mono_chord8_begin_fade.wav", (0,11))
+, ("chord5_begin_fade", "sounds/mono_chord5_begin_fade.wav", (0,11))
+, ("chord6_begin_fade", "sounds/mono_chord6_begin_fade.wav", (0,11))
+, ("chord7_begin_fade", "sounds/mono_chord7_begin_fade.wav", (0,11))
+, ("chord8_begin_fade", "sounds/mono_chord8_begin_fade.wav", (0,11))
+
+, ("chord1_begin_rev", "sounds/mono_chord1_begin_rev.wav", (0,21))
+, ("chord2_begin_rev", "sounds/mono_chord2_begin_rev.wav", (0,21))
+, ("chord3_begin_rev", "sounds/mono_chord3_begin_rev.wav", (0,21))
+, ("chord4_begin_rev", "sounds/mono_chord4_begin_rev.wav", (0,21))
+
+, ("chord5_begin_rev", "sounds/mono_chord5_begin_rev.wav", (0,21))
+, ("chord6_begin_rev", "sounds/mono_chord6_begin_rev.wav", (0,21))
+, ("chord7_begin_rev", "sounds/mono_chord7_begin_rev.wav", (0,21))
+, ("chord8_begin_rev", "sounds/mono_chord8_begin_rev.wav", (0,21))
+
+
+
+, ("mono_crotarc_d_rev", "sounds/mono_crotarc_d_rev.wav", (0,22))
+, ("mono_crotarc_e_rev", "sounds/mono_crotarc_e_rev.wav", (0,14))
+, ("mono_crotarc_a_rev", "sounds/mono_crotarc_a_rev.wav", (0,13))
+, ("mono_crotarc_b_rev", "sounds/mono_crotarc_b_rev.wav", (0,10))
 
 , ("crossing_bass", "sounds/mono_crossing_bass_with_fade.wav", (0,25))
 ]
